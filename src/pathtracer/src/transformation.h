cdbc241a1b18c5f0307062da886b0425a71c7090
priyangshu som <priyangshusom975@gmail.com>
2026-01-23 23:38:34 +0530

    c++ lib 3 transform 0

--- src/pathtracer/src/transformation.h
+++ src/pathtracer/src/transformation.h
@@ -1,4 +1,22 @@
 #ifndef TRANSFORMATION_H
 #define TRANSFORMATION_H
 
+#include "cudadefines.h"
+#include "matrix.h"
+#include "vector.h"
+#include "point.h"
+#include "normal.h"
+
+struct Transform {
+	Matrix4f matrix, invMatrix;
+	CPU_GPU Transform();
+	CPU_GPU Transform(const Matrix4f& matrix);
+	CPU_GPU Transform(const Matrix4f& matrix, const Matrix4f& invMatrix);
+	CPU_GPU static Transform Identity();
+	CPU_GPU Point3f TransformPoint(const Point3f& p);
+	CPU_GPU Point4f TransformPoint(const Point4f& p);
+	CPU_GPU Vector4f TransformVector(const Vector4f& v);
+	CPU_GPU Normal3f TransformNormal(const Normal3f& n);
+};
+
 #endif
