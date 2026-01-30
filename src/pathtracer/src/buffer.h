f1bf5b9479acd64f90095a8e1887e5e959d39462
priyangshu som <priyangshusom975@gmail.com>
2026-01-30 05:58:49 +0530

    python bindings 1

--- src/pathtracer/src/buffer.h
+++ src/pathtracer/src/buffer.h
@@ -88,21 +88,23 @@ public:
 	const SType* data() const;
 	void reserve(size_t size);
 	void push_back(const SType& elem);
-	void push_back(const SType&& elem);
-	void resize(size_t newElemCount);
+	void push_back(SType&& elem);
+	void resize(size_t newSize);
 	void assign(SType* ptr, size_t count);
 	SType& operator[](int idx);
 	const SType& operator[](int idx) const;
-	StructuredBufferView<SType> View() const;
+	StructuredBufferView<SType> view() const;
 	bool empty() const;
 	void clear();
 	~StructuredBuffer();
 };
 
+
 template<typename SType>
 void CopyData(
 	const StructuredBuffer<SType, BufferType::CPU_BUFFER>& src,
 	StructuredBuffer<SType, BufferType::CPU_BUFFER>& dst,
+	size_t start,
 	size_t nElement
 );
 
@@ -307,25 +309,67 @@ template<typename SType, BufferType type>
 StructuredBuffer<SType, type>::StructuredBuffer() : _data(nullptr), _size(0),_capacity(0)  {}
 
 template<typename SType, BufferType type>
-StructuredBuffer<SType, type>::StructuredBuffer(size_t elemCount) : _data(nullptr), _size(0), _capacity(elemCount){
-	
+StructuredBuffer<SType, type>::StructuredBuffer(size_t size) : _data(nullptr), _size(0), _capacity(0){
+	_capacity = size;
+	_size = size;
+	if (type == BufferType::CPU_BUFFER) {
+		_data = std::malloc(_size * sizeof(SType));
+		if(_data == nullptr)
+			throw StructuredBufferRuntimeError("StructuredBuffer Error: failed to allocate CPU memory");
+		SType* data = static_cast<SType*>(_data);
+		for (int i = 0; i < _size; ++i)
+			new (&data[i]) SType();
+	}
+	else if (type == BufferType::GPU_BUFFER) {
+		cudaError_t err = cudaMalloc(&_data, _size * sizeof(SType));
+		if (err != cudaSuccess)
+			throw StructuredBufferRuntimeError(cudaGetErrorString(err));
+	}
+}
+
+template<typename SType, BufferType type>
+size_t StructuredBuffer<SType, type>::size() const {
+	return _size;
+}
+
+template<typename SType, BufferType type>
+size_t StructuredBuffer<SType, type>::capacity() const {
+	return _capacity;
+}
+
+template<typename SType, BufferType type>
+SType* StructuredBuffer<SType, type>::data() {
+	return static_cast<SType*>(_data);
+}
+
+template<typename SType, BufferType type>
+const SType* StructuredBuffer<SType, type>::data() const {
+	return static_cast<SType*>(_data);
 }
 
 template<typename SType, BufferType type>
 void StructuredBuffer<SType, type>::reserve(size_t size) {
-	size_t size = length * sizeof(SType);
+	
 	if (type == BufferType::CPU_BUFFER) {
-		data = std::malloc(size);
+		void* data = std::malloc(size * sizeof(SType));
 		if (data == nullptr) {
 			throw StructuredBufferRuntimeError(
 				std::string("StructuredBuffer Error: Failed to allocate CPU buffer of size=") +=
 				std::to_string(size)
 			);
 		}
-		totalSize = size;
+		_capacity = size;
+		size_t _sizeToCopy = std::min(_capacity, _size);
+		if (_data != nullptr) {
+			std::memcpy(data, _data, _sizeToCopy * sizeof(SType));
+			std::free(_data);
+		}
+		_data = data;
+		_size = _sizeToCopy;
 	}
 	else if (type == BufferType::GPU_BUFFER) {
-		cudaError_t error = cudaMalloc(&data, size);
+		void* data;
+		cudaError_t error = cudaMalloc(&data, size * sizeof(SType));
 		if (error != cudaSuccess) {
 			throw StructuredBufferRuntimeError(
 				std::string("StructuredBuffer Error: Failed to allocate GPU buffer of size=") +=
@@ -333,89 +377,205 @@ void StructuredBuffer<SType, type>::reserve(size_t size) {
 				std::string(", CUDA error: ") + std::string(cudaGetErrorString(error))
 			);
 		}
-		totalSize = size;
+		_capacity = size;
+		size_t _sizeToCopy = std::min(_capacity, _size);
+		if (_data != nullptr) {
+			error = cudaMemcpy(data, _data, _sizeToCopy * sizeof(SType), cudaMemcpyDeviceToDevice);
+			if (error != cudaSuccess) {
+				throw StructuredBufferRuntimeError(
+					std::string("StructuredBuffer Error: Failed to allocate GPU buffer of size=") +=
+					std::to_string(size) +
+					std::string(", CUDA error: ") + std::string(cudaGetErrorString(error))
+				);
+			}
+			cudaFree(_data);
+		}
+		_data = data;
+		_size = _sizeToCopy;
 	}
 }
-#pragma endregion
-
-// StructedBuffer definition
-template<typename SType, BufferType type>
-StructuredBuffer<SType, type>::StructuredBuffer() : data(nullptr), length(0), totalSize(0), currentSize(0) {}
 
 template<typename SType, BufferType type>
-StructuredBuffer<SType, type>::StructuredBuffer(size_t elemCount) : data(nullptr), length(elemCount), totalSize(0), currentSize(0) {}
-
-template<typename SType, BufferType type>
-void StructuredBuffer<SType, type>::Allocate() {
-	size_t size = length * sizeof(SType);
+void StructuredBuffer<SType, type>::push_back(const SType& elem) {
 	if (type == BufferType::CPU_BUFFER) {
-		data = std::malloc(size);
-		if (data == nullptr) {
-			throw StructuredBufferRuntimeError(
-				std::string("StructuredBuffer Error: Failed to allocate CPU buffer of size=") +=
-				std::to_string(size)
-			);
+		if ((_capacity - _size) > 0) {
+			static_cast<SType*>(_data)[_size++] = elem;
 		}
-		totalSize = size;
+		else {
+			size_t size = 1;
+			reserve(2 * (std::max(size, _size)));
+			static_cast<SType*>(_data)[_size++] = elem;
+		}
 	}
 	else if (type == BufferType::GPU_BUFFER) {
-		cudaError_t error = cudaMalloc(&data, size);
-		if (error != cudaSuccess) {
-			throw StructuredBufferRuntimeError(
-				std::string("StructuredBuffer Error: Failed to allocate GPU buffer of size=") +=
-				std::to_string(size) +
-				std::string(", CUDA error: ") + std::string(cudaGetErrorString(error))
-			);
+		throw StructuredBufferRuntimeError("StructuredBuffer Error: push_back not available for GPU memory");
+	}
+}
+
+template<typename SType, BufferType type>
+void StructuredBuffer<SType, type>::push_back(SType&& elem) {
+	if (type == BufferType::CPU_BUFFER) {
+		if ((_capacity - _size) > 0) {
+			static_cast<SType*>(_data)[_size++] = elem;
 		}
-		totalSize = size;
+		else {
+			size_t size = 1;
+			reserve(2 * (std::max(size, _size)));
+			static_cast<SType*>(_data)[_size++] = elem;
+		}
+	}
+	else if (type == BufferType::GPU_BUFFER) {
+		throw StructuredBufferRuntimeError("StructuredBuffer Error: push_back not available for GPU memory");
+	}
+}
+
+template<typename SType, BufferType type>
+void StructuredBuffer<SType, type>::resize(size_t newSize) {
+	if (type == BufferType::CPU_BUFFER) {
+		if (newSize <= _capacity) {
+			if (_size > newSize) {
+				_size = newSize;
+			}
+			else {
+				for (int i = _size; i < newSize; ++i)
+					static_cast<SType*>(_data)[i] = SType();
+			}
+		}
+		else {
+			_capacity = newSize;
+			void* data = std::malloc(_capacity * sizeof(SType));
+			if (_data != nullptr) {
+				std::memcpy(data, _data, _size * sizeof(SType));
+				std::free(_data);
+			}
+			for (int i = _size; i < newSize; ++i)
+				static_cast<SType*>(data)[i] = SType();
+			_data = data;
+			_size = newSize;
+		}
+		
+	}
+	else if (type == BufferType::GPU_BUFFER) {
+		throw StructuredBufferRuntimeError("StructuredBuffer Error: resize not available for GPU memory");
+	}
+}
+
+template<typename SType, BufferType type>
+void StructuredBuffer<SType, type>::assign(SType* ptr, size_t count) {
+	if (type == BufferType::CPU_BUFFER) {
+		if (_data != nullptr) {
+			std::free(_data);
+			_size = 0;
+			_capacity = 0;
+		}
+		_data = static_cast<void*>(ptr);
+		_size = count;
+		_capacity = count;
+	}
+	else if (type == BufferType::GPU_BUFFER) {
+		if (_data != nullptr) {
+			cudaFree(_data);
+			_size = 0;
+			_capacity = 0;
+		}
+		_data = static_cast<void*>(ptr);
+		_size = count;
+		_capacity = count;
 	}
 }
 
 template<typename SType, BufferType type>
 SType& StructuredBuffer<SType, type>::operator[](int idx) {
+	if (type == BufferType::CPU_BUFFER) {
 #ifdef DEBUG
-	if (idx<0 || idx>=length)
-		throw StructuredBufferRuntimeError("StructuredBuffer Error: Index out of bound");
+		if (idx < 0 || idx >= _size)
+			throw StructuredBufferRuntimeError("StructuredBuffer Error: Index out of bound");
 #endif
-	return *(static_cast<SType*>(data) + idx);
-
+		return *(static_cast<SType*>(_data) + idx);
+	}
+	else if (type == BufferType::GPU_BUFFER)
+		throw StructuredBufferRuntimeError("StructuredBuffer Error: StructuredBuffer indexing not available for GPU memory\nGPU memory indexing available through StructuredBufferView");
 }
 
 template<typename SType, BufferType type>
 const SType& StructuredBuffer<SType, type>::operator[](int idx) const {
+	if (type == BufferType::CPU_BUFFER) {
 #ifdef DEBUG
-	if (idx<0 || idx>=length)
-		throw StructuredBufferRuntimeError("StructuredBuffer Error: Index out of bound");
+		if (idx < 0 || idx >= _size)
+			throw StructuredBufferRuntimeError("StructuredBuffer Error: Index out of bound");
 #endif
-	return *(static_cast<SType*>(data) + idx);
+		return *(static_cast<SType*>(_data) + idx);
+	}
+	else if(type == BufferType::GPU_BUFFER)
+		throw StructuredBufferRuntimeError("StructuredBuffer Error: StructuredBuffer indexing not available for GPU memory\nGPU memory indexing available through StructuredBufferView");
 }
 
 template<typename SType, BufferType type>
-StructuredBufferView<SType> StructuredBuffer<SType, type>::View() const {
-	return { static_cast<SType*>(data), length };
+StructuredBufferView<SType> StructuredBuffer<SType, type>::view() const {
+	return { static_cast<SType*>(_data),_size };
+}
+
+template<typename SType, BufferType type>
+bool StructuredBuffer<SType, type>::empty() const {
+	return _size == 0;
+}
+
+template<typename SType, BufferType type>
+void StructuredBuffer<SType, type>::clear() {
+	if (type == BufferType::CPU_BUFFER) {
+		if (_data != nullptr) {
+			SType* data = static_cast<SType*>(_data);
+			for (int i = 0; i < _size; ++i) {
+				(&data[i])->~SType();
+			}
+		}
+		_size = 0;
+	}
+	else if (type == BufferType::GPU_BUFFER) {
+		throw StructuredBufferRuntimeError("StructuredBuffer Error: clear not available for GPU memory");
+	}
+}
+
+template<typename SType, BufferType type>
+StructuredBuffer<SType, type>::~StructuredBuffer() {
+	if (type == BufferType::CPU_BUFFER) {
+		if (_data) {
+			SType* data = static_cast<SType*>(_data);
+			for (size_t i = 0; i < _size; ++i) {
+				data[i].~SType();
+			}
+			std::free(_data);
+		}
+	}
+	else if (type == BufferType::GPU_BUFFER)
+		cudaFree(_data);
+	_size = 0;
+	_capacity = 0;
 }
 
 template<typename SType>
 void CopyData(
 	const StructuredBuffer<SType, BufferType::CPU_BUFFER>& src,
 	StructuredBuffer<SType, BufferType::CPU_BUFFER>& dst,
+	size_t start,
 	size_t nElements
 ) {
 	size_t bytes = nElements * sizeof(SType);
-	if (bytes > dst.totalSize - dst.currentSize) {
+	if (nElements > dst.capacity() - dst.size()) {
 		throw StructuredBufferRuntimeError(
 			"StructuredBuffer Error: Not enough space in destination CPU buffer" +
-			std::string(", requested ") + std::to_string(bytes) +
-			std::string(", available ") + std::to_string(dst.totalSize - dst.currentSize)
+			std::string(", requested count ") + std::to_string(nElements) +
+			std::string(", available count ") + std::to_string(dst.capacity() - dst.size())
 		);
 	}
-	char* dstPtr = static_cast<char*>(dst.data) + dst.currentSize;
-	std::memcpy((void*)dstPtr, src.data, bytes);
-	dst.currentSize += bytes;
+	size_t _sizeToCopy = nElements;
+	SType* dstPtr = &dst[0] + dst.size();
+	SType* srcPtr = &dst[0] + start;
+	std::memcpy((void*)dstPtr, (void*)srcPtr, _sizeToCopy*sizeof(SType));
 }
 
 template<typename SType>
- void CopyData(
+void CopyData(
 	const StructuredBuffer<SType, BufferType::GPU_BUFFER>& src,
 	StructuredBuffer<SType, BufferType::GPU_BUFFER>& dst,
 	size_t nElements
@@ -442,7 +602,7 @@ template<typename SType>
 }
 
 template<typename SType>
- void CopyData(
+void CopyData(
 	const StructuredBuffer<SType, BufferType::CPU_BUFFER>& src,
 	StructuredBuffer<SType, BufferType::GPU_BUFFER>& dst,
 	size_t nElements
@@ -479,7 +639,7 @@ void CopyData(
 		throw StructuredBufferRuntimeError(
 			"StructuredBuffer Error: Not enough space in destination CPU buffer" +
 			std::string(", requested ") + std::to_string(bytes) +
-			std::string (", available ") + std::to_string(dst.totalSize - dst.currentSize)
+			std::string(", available ") + std::to_string(dst.totalSize - dst.currentSize)
 		);
 	}
 	char* dstPtr = static_cast<char*>(dst.data) + dst.currentSize;
@@ -495,16 +655,6 @@ void CopyData(
 	dst.currentSize += bytes;
 }
 
-template<typename SType, BufferType type>
- StructuredBuffer<SType, type>::~StructuredBuffer() {
-	if (data != nullptr) {
-		if (type == BufferType::CPU_BUFFER) {
-			std::free(data);
-		}
-		else if (type == BufferType::GPU_BUFFER) {
-			cudaFree(data);
-		}
-	}
-}
+#pragma endregion
 
 #endif
--- src/pathtracer/src/soa.h
+++ src/pathtracer/src/soa.h
@@ -6,9 +6,14 @@
 #include "vector.h"
 #include "point.h"
 #include "normal.h"
+#include "triangle.h"
+#include "mesh.h"
 
 struct SOA {
-
+	StructuredBuffer<Vector3i,BufferType::CPU_BUFFER> indices;
+	StructuredBuffer<Point3f, BufferType::CPU_BUFFER> positions;
+	StructuredBuffer<Normal3f, BufferType::CPU_BUFFER> normals;
+	StructuredBuffer<Vector2f, BufferType::CPU_BUFFER> uvs;
 };
 
 #endif
